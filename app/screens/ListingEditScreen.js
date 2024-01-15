452869e9154e7e27561a617ceee1822002b09d13
Janika Savela <janika_s@hotmail.com>
2024-01-15 15:04:55 +0200

    kuvan lähettäminen tietokantaan onnistuu

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, ActivityIndicator, Alert } from "react-native";
 import * as Yup from "yup";
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
 
 import {
   AppForm as Form,
@@ -12,16 +13,23 @@ import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
 import useLocation from "../hooks/useLocation";
-import { collection, firestore, query, addDoc, serverTimestamp, getDoc } from "../firebase/Config";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  collection,
+  firestore,
+  addDoc,
+  serverTimestamp,
+  getDoc,
+  updateDoc
+} from "../firebase/Config";
 import colors from "../config/colors";
+
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
-  images: Yup.array().min(1, "Please select at least one image.")
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
@@ -82,40 +90,90 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
 
-  const location = useLocation()
-  const [loading, setLoading] = useState(false)
+  const uploadImagesToStorage = async (images, id) => {
+    const imageUrls = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      const imageUrl = await uploadImageAsync(image, id, i);
+      imageUrls.push(imageUrl);
+    }
+
+    return imageUrls;
+  };
+
+  const uploadImageAsync = async (uri, id, index) => {
+    try {
+      const buffer = await urlToBuffer(uri);
+      const timestamp = new Date().getTime();
+      const fileExtension = uri.split(".").pop();
+
+      const fileName = `${timestamp}_${index}.${fileExtension}`;
+      const fileRef = ref(getStorage(), `ilmoitukset/${id}/${fileName}`);
+      await uploadBytes(fileRef, buffer);
+
+      return fileName;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  const urlToBuffer = async (uri) => {
+    try {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      return blob;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
 
   const addReport = async (reportinfo, { resetForm }) => {
     try {
-      setLoading(true); // Näytä latausindikaattori
-    
-        // Lisää dokumentti Firestoreen ja hae sen ID
-        const addedDocRef = await addDoc(collection(firestore, "ilmoitukset"), {
-          created: serverTimestamp(),
-          categoryId: reportinfo.category.value,
-          price: reportinfo.price,
-          title: reportinfo.title,
-          description: reportinfo.description,
-        });
-  
-        // Hae lisätyn dokumentin tiedot käyttämällä dokumentin ID:tä
-        const addedDocSnapshot = await getDoc(addedDocRef);
-  
-        // Tarkista, että dokumentti on lisätty onnistuneesti
-        if (addedDocSnapshot.exists()) {
-          Alert.alert('Sending was successful', 'Form sent successfully.');
-        } else {
-          Alert.alert('Error', 'Form submission failed, please try again.');
+      setLoading(true);
+
+      // Add document to Firestore and get its ID
+      const addedDocRef = await addDoc(collection(firestore, "ilmoitukset"), {
+        created: serverTimestamp(),
+        categoryId: reportinfo.category.value,
+        price: reportinfo.price,
+        title: reportinfo.title,
+        description: reportinfo.description,
+        images: reportinfo.images,
+      });
+
+      const addedDocSnapshot = await getDoc(addedDocRef);
+
+      if (addedDocSnapshot.exists()) {
+        if (reportinfo.images.length > 0) {
+          const imageUrls = await uploadImagesToStorage(
+            reportinfo.images,
+            addedDocSnapshot.id
+          );
+
+          // Update Firestore document with image URLs
+          await updateDoc(addedDocRef, {
+            images: imageUrls,
+          });
         }
+
+        Alert.alert("Sending was successful", "Form sent successfully.");
+      } else {
+        Alert.alert("Error", "Form submission failed, please try again.");
+      }
     } catch (error) {
       console.log(error);
-      Alert.alert('Error', 'An error occurred, please try again.');
+      Alert.alert("Error", "An error occurred, please try again.");
     } finally {
-      setLoading(false); // Piilota latausindikaattori
+      setLoading(false);
       resetForm();
     }
-    console.log("lomaketiedot", reportinfo);
+    console.log("Form data", reportinfo);
   };
 
   return (
@@ -126,12 +184,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
-          images: []
+          images: [],
         }}
         onSubmit={addReport}
         validationSchema={validationSchema}
       >
-        <FormImagePicker name="images"/>
+        <FormImagePicker name="images" />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
@@ -140,7 +198,14 @@ function ListingEditScreen() {
           placeholder="Price"
           width={120}
         />
-        <Picker items={categories} name="category" placeholder="Category" width="50%" PickerItemComponent={CategoryPickerItem} numberOfColumns={3}/>
+        <Picker
+          items={categories}
+          name="category"
+          placeholder="Category"
+          width="50%"
+          PickerItemComponent={CategoryPickerItem}
+          numberOfColumns={3}
+        />
         <FormField
           maxLength={255}
           multiline
@@ -149,7 +214,13 @@ function ListingEditScreen() {
           placeholder="Description"
         />
         <SubmitButton title="Post" />
-        {loading && <ActivityIndicator size="large" animating={true} color={colors.primary}/>}
+        {loading && (
+          <ActivityIndicator
+            size="large"
+            animating={true}
+            color={colors.primary}
+          />
+        )}
       </Form>
     </Screen>
   );
@@ -160,4 +231,5 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
+
 export default ListingEditScreen;
