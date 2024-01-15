ea3b407490e2029ec41a70a2de911f6a6181780b
Janika Savela <janika_s@hotmail.com>
2024-01-15 18:59:09 +0200

    kaikki toimii

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image, View, StyleSheet } from 'react-native';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+
 
 import AppText from '../components/AppText';
 import ListItem from '../components/ListItem';
@@ -8,10 +10,27 @@ import colors from '../config/colors';
 function ListingDetailsScreen({ route }) {
 
 const listing = route.params
+const [imageUrl, setImageUrl] = useState()
+const storage = getStorage()
+const ilmRef = ref(storage, "ilmoitukset/"+listing.id)
+const filename = listing.images[0] ? ref(ilmRef, listing.images[0]) : null;
+const filePath = filename ? filename.fullPath : null;
+
+if (filePath) {
+    console.log("FILEPATH: " ,filePath)
+    getDownloadURL(ref(storage, filePath))
+    .then((url) => {
+        setImageUrl(url)
+        console.log("IMAGEURL", imageUrl)
+        console.log("IMAGE", listing.images[0])
+    }).catch((error) => { console.log("ERROR", error)})
+}
 
     return (
         <View>
- <Image style={styles.image} source={listing.image}/>
+{imageUrl &&
+   <Image style={styles.image} source={{uri: imageUrl}}/>
+}
  <View style={styles.detailsContainer}>
  <AppText style={styles.title}>{listing.title}</AppText>
  <AppText style={styles.desc}>{listing.description}</AppText>
